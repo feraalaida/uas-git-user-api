22cc334a175998a8ec4d9e5f1cb3e9c8f154d4a1
fera alaida syeliana 2399 <fera.alaida@gmail.com>
2021-01-18 08:46:37 +0700

    first commit

---
+++ app/src/main/java/com/appserba/api/DetailActivity.kt
@@ -0,0 +1,17 @@
+package com.appserba.api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        supportActionBar?.apply {
+            elevation = 0f
+            title = ""
+
+        }
+    }
+}
